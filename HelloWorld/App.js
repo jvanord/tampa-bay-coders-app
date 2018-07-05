3466262d0491f79e65bd1dc3b718cb532c448d68
Jay VanOrd <jvanord@indasysllc.com>
2018-07-05 10:46:52 -0400

    refactored home page to it's own component

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,63 +1,11 @@
 import React from 'react';
-import {StyleSheet, Text, View, Vibration, Alert, Linking, Button, Image } from 'react-native';
-import {Card} from 'react-native-elements';
+import {StyleSheet} from 'react-native';
+import HomeComponent from './components/Home';
 
 export default class App extends React.Component {
-	componentWillMount() {
-		//this.alertAndVibrate(1000);
-		console.log('App Mounted');
-	}
-	onAlertPressed = () => {
-		this.alertAndVibrate(5000);
-	}
-	alertAndVibrate = (delay) => {
-		setTimeout(function(){
-			Vibration.vibrate([300, 300, 1000], true);
-			Alert.alert(
-				'Hello from Indasys',
-				'This is just a simple app to test the quick creation and deployment of cross-development apps. It doesn\'t do anything. Well, it opens our website - that\'s something.',
-				[
-					//{
-					//	text: 'Ask me later',
-					//	onPress: () => console.log('Ask me later pressed')
-					//},
-					{
-						text: 'Cancel',
-						onPress: () => { Vibration.cancel(); console.log('Cancel Pressed'); },
-						style: 'cancel'
-					},
-					{text: 'OK', onPress: () => { Vibration.cancel(); console.log('OK Pressed'); }}
-				],
-				{cancelable: false}
-			);
-		}, delay || 1000); 
-	}
 	render() {
 		return (
-			<View style={styles.container}>
-				<View style={{marginBottom:20}}>
-					<Image source={require('./assets/images/logo.png')} />
-				</View>
-				<Card title="Custom Apps by Indasys" style={[{color: '#009', justifyContent: 'center', alignItems: 'center'}]}>
-					<Text>
-						Indasys is a customer software and app devloper with locations in Saint Petersburg, FL, and New York, NY.
-						Indasys builds line of business apps for the browser and all major mobile platforms.
-					</Text>
-					<Text style={{color: '#009', display: 'none' }} onPress={() => Linking.openURL('http://www.indasysllc.com/')}>
-						Go To Indasys Website
-					</Text>
-					<View style={{margin: 20}}>
-						<Button title="VIEW INDASYS WEBSITE" containerViewStyle={{margin: '10'}} onPress={() => Linking.openURL('http://www.indasysllc.com/')} />
-					</View>
-				</Card>
-				<View style={{margin:10,justifyContent:'center',alignItems: 'center'}}>
-					<Text>Developer Instructions:</Text>
-					<Text>Open up App.js to start working on your app!</Text>
-					<Text>Changes you make will automatically reload.</Text>
-					<Text>Shake your phone to open the developer menu.</Text>
-				</View>
-				<Button title="TEST ALERT (5 second delay)" onPress={this.onAlertPressed} />
-			</View>
+			<HomeComponent styles={styles} />
 		);
 	}
 }
@@ -68,11 +16,5 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center'
-	},
-	hello: {
-		flex: 1,
-		backgroundColor: '#009',
-		color: '#009',
-		marginBottom: '20'
 	}
 });
